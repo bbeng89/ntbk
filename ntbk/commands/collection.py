a00cbe654f4574054cad63bcf2b35fa288376e2e
Blake Bengtson <blake@bengtson.us>
2021-12-16 16:46:23 -0600

    Added count of files in collection and color coded - blue if one file, green if greater than one

--- ntbk/commands/collection.py
+++ ntbk/commands/collection.py
@@ -1,19 +1,28 @@
 # system imports
 from pathlib import Path
 
+# 3rd party imports
+from colorama import Fore, Style
+
 # app imports
 import config
 
 def filepath_for_collection(collection, file):
     return f'collections/{collection}/{file}.md'
 
+def get_files_in_collection(collection_path):
+    files = []
+    for child in collection_path.glob('*.*'):
+        files.append(child)
+    return files
+
 def list_collections(args):
     conf = config.load_config()
     collection_dir = Path(conf['ntbk_dir']).expanduser() / 'collections'
+
     for child in collection_dir.glob('*/'):
-        filecount = 0
-        for f in child.glob('*.*'):
-            filecount += 1
-        countstr = f'{filecount} {"file" if filecount == 1 else "files"}'
-        print(f'{child.relative_to(collection_dir)} [{countstr}]')
+        files = get_files_in_collection(child)
+        countstr = f'{len(files)} {"file" if len(files) == 1 else "files"}'
+        color = Fore.BLUE if len(files) == 1 else Fore.GREEN
+        print(f'{child.relative_to(collection_dir)} {color}[{countstr}]{Style.RESET_ALL}')
 
