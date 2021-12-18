c98eafdefc021f73708fefd0fc50a84faf002caf
Blake Bengtson <blake@bengtson.us>
2021-12-18 00:26:54 -0600

    Documented invalid config exception

--- ntbk/exceptions.py
+++ ntbk/exceptions.py
@@ -1,5 +1,6 @@
 
 class InvalidConfigException(Exception):
+    """Exception that is thrown if the configuration file is not valid"""
 
     def __init__(self):
         super().__init__('Configuration file is not valid. Please review the docs and check your config file.')
