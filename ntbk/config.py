aa672fc3ec28fa2997b860de23b894674973c49c
Blake Bengtson <blake@bengtson.us>
2021-12-18 00:20:07 -0600

    Documented config file

--- ntbk/config.py
+++ ntbk/config.py
@@ -6,6 +6,7 @@ import yaml
 
 
 class Config():
+    """This file is a wrapper around the config file that makes it easy to get/set values and update the config file"""
 
     CONFIG_FILEPATH = '~/.config/ntbk/ntbk.yml'
 
@@ -33,31 +34,38 @@ class Config():
         self.load()
 
     def get(self, key, default=None):
+        """Get the value from the config for the given key"""
         return self._config.get(key, default)
 
     def set(self, key, value):
+        """Set the value in the config for the given key. The file will be immediately saved"""
         self._config[key] = value
         self.save()
 
     def save(self):
+        """Write the config to disk"""
         self._config_path.parent.mkdir(parents=True, exist_ok=True)
         self._config_path.write_text(yaml.dump(self._config))
 
     def is_valid(self):
+        """Whether or not the config file is valid"""
         for key in self.REQUIRED_KEYS:
             if key not in self._config or not self._config[key]:
                 return False
         return True
 
     def load(self):
+        """Load the config yaml from disk into the config class level dict variable"""
         if self.config_file_exists():
             with self._config_path.open() as file:
                 self._config = yaml.load(file, Loader=yaml.FullLoader)
 
     def config_file_exists(self):
+        """Whether or not the config file exists on disk"""
         return self._config_path.exists()
 
     def reset_to_defaults(self):
+        """Restore the config file to the original defaults defined in DEFAULTS"""
         self._config = self.DEFAULTS
         self.save()
 
