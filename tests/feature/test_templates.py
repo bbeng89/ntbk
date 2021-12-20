6df2582b3bee17c4be78384c8d2a780b6f6f7ca3
Blake Bengtson <blake@bengtson.us>
2021-12-19 19:38:57 -0600

    Realized i was still passing a template arg. removed that. tests still pass

--- tests/feature/test_templates.py
+++ tests/feature/test_templates.py
@@ -5,7 +5,7 @@ def test_creating_new_log_with_default_template(dispatcher, ntbk_dir, simple_tem
     t_name = simple_template.get_name()
     dispatcher.config.set('default_templates', {'log': { 'index': t_name }})
     expected_file = ntbk_dir / 'log/2021/01-january/2021-01-01/index.md'
-    dispatcher.run(['today', '--template', t_name])
+    dispatcher.run(['today'])
     assert expected_file.read_text() == '# Simple Template'
     dispatcher.filesystem.open_file_in_editor.assert_called_with(expected_file)
 
@@ -13,7 +13,7 @@ def test_creating_new_collection_with_default_template(dispatcher, ntbk_dir, sim
     t_name = simple_template.get_name()
     dispatcher.config.set('default_templates', {'collection': { 'books': t_name }})
     expected_file = ntbk_dir / 'collections/books/1984.md'
-    dispatcher.run(['collection', 'books', '1984', '--template', t_name])
+    dispatcher.run(['collection', 'books', '1984'])
     assert expected_file.read_text() == '# Simple Template'
     dispatcher.filesystem.open_file_in_editor.assert_called_with(expected_file)
 
