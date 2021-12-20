5712e486298cbfcda74eb183d4fae5cdb02113b8
Blake Bengtson <blake@bengtson.us>
2021-12-19 19:38:10 -0600

    Added test for creating collection with default template

--- tests/feature/test_templates.py
+++ tests/feature/test_templates.py
@@ -5,15 +5,17 @@ def test_creating_new_log_with_default_template(dispatcher, ntbk_dir, simple_tem
     t_name = simple_template.get_name()
     dispatcher.config.set('default_templates', {'log': { 'index': t_name }})
     expected_file = ntbk_dir / 'log/2021/01-january/2021-01-01/index.md'
-
     dispatcher.run(['today', '--template', t_name])
-
     assert expected_file.read_text() == '# Simple Template'
     dispatcher.filesystem.open_file_in_editor.assert_called_with(expected_file)
 
-
-def test_creating_new_collection_with_default_template():
-    pass
+def test_creating_new_collection_with_default_template(dispatcher, ntbk_dir, simple_template):
+    t_name = simple_template.get_name()
+    dispatcher.config.set('default_templates', {'collection': { 'books': t_name }})
+    expected_file = ntbk_dir / 'collections/books/1984.md'
+    dispatcher.run(['collection', 'books', '1984', '--template', t_name])
+    assert expected_file.read_text() == '# Simple Template'
+    dispatcher.filesystem.open_file_in_editor.assert_called_with(expected_file)
 
 def test_non_empty_file_not_overwritten():
     pass
