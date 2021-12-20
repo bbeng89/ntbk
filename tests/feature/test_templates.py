3b2e2343dfded3ce64b764d60b04a3d7d6357116
Blake Bengtson <blake@bengtson.us>
2021-12-19 20:11:20 -0600

    Use template.render() instead of hardcoding

--- tests/feature/test_templates.py
+++ tests/feature/test_templates.py
@@ -6,7 +6,7 @@ def test_creating_new_log_with_default_template(dispatcher, ntbk_dir, simple_tem
     dispatcher.config.set('default_templates', {'log': { 'index': t_name }})
     expected_file = ntbk_dir / 'log/2021/01-january/2021-01-01/index.md'
     dispatcher.run(['today'])
-    assert expected_file.read_text() == '# Simple Template'
+    assert expected_file.read_text() == simple_template.render()
     dispatcher.filesystem.open_file_in_editor.assert_called_with(expected_file)
 
 def test_creating_new_collection_with_default_template(dispatcher, ntbk_dir, simple_template):
@@ -14,7 +14,7 @@ def test_creating_new_collection_with_default_template(dispatcher, ntbk_dir, sim
     dispatcher.config.set('default_templates', {'collection': { 'books': t_name }})
     expected_file = ntbk_dir / 'collections/books/1984.md'
     dispatcher.run(['collection', 'books', '1984'])
-    assert expected_file.read_text() == '# Simple Template'
+    assert expected_file.read_text() == simple_template.render()
     dispatcher.filesystem.open_file_in_editor.assert_called_with(expected_file)
 
 def test_non_empty_file_not_overwritten(dispatcher, collection_file, simple_template):
@@ -26,6 +26,17 @@ def test_non_empty_file_not_overwritten(dispatcher, collection_file, simple_temp
     # make sure the existing text is not overwritten by the default template
     assert collection_file.get_path().read_text() == 'Placeholder text'
     dispatcher.filesystem.open_file_in_editor.assert_called_with(collection_file.get_path())
+
+# def test_empty_file_is_overwritten(dispatcher, collection_file, simple_template):
+#     t_name = simple_template.get_name()
+#     c_name = collection_file.get_collection().get_name()
+#     dispatcher.config.set('default_templates', {'collection': { c_name: t_name }})
+#     # newlines and spaces should to be ignored
+#     collection_file.get_path().write_text(' \n  \n ')
+#     dispatcher.run(['collection', c_name, collection_file.get_name()])
+#     assert collection_file.get_path().read_text() == '# Simple Template'
+#     dispatcher.filesystem.open_file_in_editor.assert_called_with(collection_file.get_path())
+
 
 def test_creating_log_with_template_arg():
     pass
