6f597dad1d9ec4c3474a92d318acc78c02c65db2
Blake Bengtson <blake@bengtson.us>
2022-01-26 10:48:03 -0600

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'ntbk',
-    version = '0.1.2',
+    version = '0.2.0',
     author = 'Blake Bengtson',
     author_email = 'blake@bengtson.us',
     license = 'MIT',
